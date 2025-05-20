7a832880428d137f3b312d9c1b8dff61aef1cb2c
Nemausa <tappan.billz@gmail.com>
2025-05-20 15:50:45 +0800

    x

---
+++ depends/include/network.hpp
@@ -0,0 +1,84 @@
+/**
+ * @file network.hpp
+ *
+ * @brief This message displayed in Doxygen Files index
+ *
+ * @ingroup PackageName
+ * (note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Nemausa
+ */
+
+#ifndef NETWORK_H_
+#define NETWORK_H_
+
+#include "cell.h"
+
+#ifdef _WIN32
+#include <windsock2.h>
+#else
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#endif
+
+constexpr int kOn = 1;
+#ifdef _WIN32
+constexpr WORD kWinSockVersion = MAKEWORD(2, 2);
+#endif
+
+class Netowrk {
+ public:
+  static void Init() {
+#ifdef _WIN32
+    WSADATA was_data;
+    WSAStartup(kWinSockVersion, &wsa_data);
+#else
+    signal(SIGPIPE, SIG_IGN);
+#endif
+  }
+
+  static bool MakeNonBlocking(SOCKET fd) noexcept {
+#ifdef _WIN32
+    unsigned long mode = kOn;
+    if (ioctlsocek(fd, FIONBIN, &mode) == SOCKET_ERROR) {
+      LOG(WARNING) << "ioctlsocek failed for" << fd;
+      return false;
+    }
+#else
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0 || (fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0)) {
+      LOG(WARNING) << "fcntl non-blocking failed for " << fd;
+      return false;
+    }
+#endif
+    return true;
+  }
+
+  static bool MakeReuseAddr(SOCKET fd) noexcept {
+    return SetSockOpt(fd, SOL_SOCKET, SO_REUSEADDR);
+  }
+  static bool MakeNoDelay(SOCKET fd) noexcept {
+    return SetSockOpt(fd, IPPROTO_TCP, TCP_NODELAY);
+  }
+
+  static void DestroySocket(SOCKET fd) noexcept {
+#if _WIN32
+    closesocked(fd);
+#else
+    close(fd);
+#endif  // _WIN32
+  }
+
+ private:
+  static bool SetSockOpt(SOCKET fd, int level, int optname) noexcept {
+    if (setsockopt(fd, level, optname, reinterpret_cast<const char*>(&kOn),
+                   sizeof(kOn)) == SOCKET_ERROR) {
+      LOG(WARNING) << "setsockopt " << fd < " opt " << opt << " failed";
+      return false;
+    }
+    return true;
+  }
+};
+
+#endif  // NETWORK_H_
---
+++ depends/src/config.cpp
@@ -0,0 +1,81 @@
+#include "config.hpp"
+#include <cstdlib>
+#include <memory>
+#include <system_error>
+
+void Config::Init(int argc, char* argv[]) noexcept {
+  if (argc > 0) {
+    exe_path_ = argv[0];
+  }
+  for (int i = 1; i < argc; ++i) {
+    Parse(argv[i]);
+  }
+}
+
+void Config::Parse(const std::string& cmd) noexcept {
+  auto pos = cmd.find('=');
+  if (pos != std::string::npos) {
+    std::string key = cmd.substr(0, pos);
+    std::string val = cmd.substr(pos + 1);
+    kv_[key] = val;
+    LOG(INFO) << "Config parsed " << key << " " << val;
+  } else {
+    kv_[cmd] = "";
+    LOG(INFO) << "Config parsed flag " << cmd;
+  }
+}
+
+bool Config::HasKey(const std::string& key) const noexcept {
+  return kv_.count(key) > 0;
+}
+
+std::string Config::GetString(const std::string& key,
+                              const std::string& default_value) const {
+  auto it = kv_.find(key);
+  if (it == kv_.end()) {
+    LOG(INFO) << "Config GetString " << key
+              << " not found, use default=" << default_value;
+    return default_value;
+  }
+  LOG(INFO) << "Config GetString " << key << "=" << it->second;
+  return it->second;
+}
+
+int Config::GetInt(const std::string& key, int default_value) const noexcept {
+  auto it = kv_.find(std::string(key));
+  if (it != kv_.end()) {
+    int v = std::atoi(it->second.c_str());
+    LOG(INFO) << key << "=" << v;
+    return v;
+  }
+  return default_value;
+}
+
+bool Config::GetBool(const std::string& key,
+                     bool default_value) const noexcept {
+  auto str = GetString(key, default_value ? "true" : "false");
+  if (str == "1" || str == "true" || str == "True") return true;
+  if (str == "0" || str == "false" || str == "False") return false;
+  LOG(ERROR) << "Config GetBool invalid boolean for " << key << " " << str;
+  return default_value;
+}
+
+double Config::GetDouble(const std::string& key,
+                         double default_value) const noexcept {
+  auto it = kv_.find(std::string(key));
+  if (it == kv_.end()) {
+    LOG(INFO) << "Config GetDouble " << key << " not found, use default "
+              << default_value;
+    return default_value;
+  }
+  double result = default_value;
+  try {
+    result = std::stod(it->second);
+  } catch (const std::exception& e) {
+    LOG(ERROR) << "Config GetDouble: invalid double for " << key << " = "
+               << it->second.c_str();
+    result = default_value;
+  }
+  LOG(INFO) << "Config GetDouble: " << key << " = " << result;
+  return result;
+}
